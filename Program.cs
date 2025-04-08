62e6794b1e64cefe7ea928ce505aa035696475b2
Douglas Crawford <doug.a.crawford@gmail.com>
2025-04-08 11:49:37 -0700

    Basic MSAL authentication

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Identity.Client;
+using System.Threading.Tasks;
+
+class Program
+{
+    private const string _clientId = "";
+    private const string _tenantId = "";
+    public static async Task Main(string[] args)
+    {
+        var app = PublicClientApplicationBuilder
+            .Create(_clientId)
+            .WithAuthority(AzureCloudInstance.AzurePublic, _tenantId)
+            .WithRedirectUri("http://localhost")
+            .Build();
+
+        string[] scopes = { "User.Read" };
+
+        AuthenticationResult result = await app.AcquireTokenInteractive(scopes).ExecuteAsync();
+
+        Console.WriteLine($"Token: \t{result.AccessToken}");
+    }
+}
+
